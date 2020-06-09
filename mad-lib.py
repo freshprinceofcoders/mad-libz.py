020442ae471993f623d4f5256d7187b84fbc9a82
freshprinceofcoders <milanosolanki27@gmail.com>
2020-06-09 12:44:06 +0100

    first-game

--- mad-lib.py
+++ mad-lib.py
@@ -1,1 +1,16 @@
+noun = input("Enter a noun:")
 
+colour = input("Enter a colour:")
+
+plural_noun = input("Enter a plural noun:")
+
+another_colour = input("Enter a another colour:")
+
+animal = input("Enter a animal:")
+
+
+
+print(noun + " is " + colour)
+print(plural_noun + " are "+ another_colour)
+print(animal + " are fat ")
+print("and so are you")
