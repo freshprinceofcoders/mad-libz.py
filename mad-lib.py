33b39fd6633471d46d754fc55590e93d3d634e46
freshprinceofcoders <milanosolanki27@gmail.com>
2020-06-09 13:26:15 +0100

    formated string

--- mad-lib.py
+++ mad-lib.py
@@ -8,9 +8,14 @@ another_colour = input("Enter a another colour:")
 
 animal = input("Enter a animal:")
 
+f"{noun} is {colour}"
+f"{plural_noun} ara {another_colour}"
+  
 
 
+'''
 print(noun + " is " + colour)
 print(plural_noun + " are "+ another_colour)
 print(animal + " are fat ")
 print("and so are you")
+'''
